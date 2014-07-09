0f8211aad27f96ee3eaa105a4989ddcf1ec4edc6
ntalbs <ntalbsen@gmail.com>
2014-07-09 14:37:53 +0900

    add js and css files.

---
+++ resources/public/js/count.js
@@ -0,0 +1,23 @@
+$(function () {
+  $('#msg').focus();
+
+  var update = function () {
+    var msgCnt = $('#msg').val().length,
+        srcCnt = $('#src').val().length,
+        count = msgCnt + srcCnt + (srcCnt > 0 ? 1 : 0);
+    $('#count').text(count);
+  };
+
+  $('#msg').on('keyup', function (e) {
+    update();
+  });
+  $('#src').on('keyup', function (e) {
+    update();
+  });
+
+  $('form').submit(function (e) {
+    $('#msg').val('').focus();
+    $('#src').val('');
+    return false;
+  });
+});
