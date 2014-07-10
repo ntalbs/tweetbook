33cafed192082640d9652aa3f8dc3243d60ff4f0
ntalbs <ntalbsen@gmail.com>
2014-07-10 10:20:32 +0900

    Update home routes.

--- src/tweetbook/routes/home.clj
+++ src/tweetbook/routes/home.clj
@@ -1,16 +1,24 @@
 (ns tweetbook.routes.home
   (:require [compojure.core :refer :all]
-            [hiccup.form :refer :all]
-            [tweetbook.views.layout :as layout]))
+            [liberator.core :refer [defresource resource request-method-in]]
+            [noir.io :as io]
+            [clojure.java.io :refer [file]]))
 
-(defn show-form []
-  (form-to [:post "/"]
-           (text-area {:rows 5 :cols 50} "msg") [:br]
-           (text-field "src") [:br]
-           [:p#count "0"]))
+(defresource home
+  :available-media-types ["text/html"]
 
-(defn home []
-  (layout/common (show-form)))
+  :exists?
+  (fn [_]
+    [(io/get-resource "/index.html")
+     {::file (file (str (io/resource-path) "/index.html"))}])
+
+  :handle-ok
+  (fn [{{{resource :resource} :route-params} :request}]
+    (clojure.java.io/input-stream (io/get-resource "/index.html")))
+
+  :last-modified
+  (fn [{{{resource :resource} :route-params} :request}]
+    (.lastModified (file (str (io/resource-path) "/index.html")))))
 
 (defroutes home-routes
-  (GET "/" [] (home)))
+  (ANY "/" [] home))
