11a2a44affa6b7672b3998a2e5bdc724adfc111e
ntalbs <ntalbsen@gmail.com>
2014-07-11 19:07:04 +0900

    마크업 변경. 스크립트 수정.

--- resources/public/js/tweetbook.js
+++ resources/public/js/tweetbook.js
@@ -8,22 +8,24 @@ $(function () {
     $('#count').text(140 - count);
   };
 
-  $('#msg').on('keyup', function (e) {
-    update();
-  });
-  $('#src').on('keyup', function (e) {
-    update();
-  });
+  $('#msg, #src').on('keyup', function (e) { update(); });
 
   $('form').submit(function (e) {
-    $.post(
-      "/addmesg",
-      { mesg: $('#msg').val(), src: $('#src').val(), "tweet-immediately":  $('#tweet-immediately').val()}
-    ).done(function (data) {
+    var quote = {
+      msg: $('#msg').val(),
+      src: $('#src').val(),
+      "tweet-immediately":  $('#tweet-immediately').val()
+    };
+    var $listItem = $('<li>'+quote.msg+'<br>'+quote.src +'</li>');
+
+    $listItem.appendTo($('#list-container')).css({color: "gray"});
+
+    $.post("/addmesg", quote).done(function (data) {
       console.log(data);
       $('#msg').val('').focus();
       $('#src').val('');
       $('#tweet-immediately').val('');
+      $listItem.css({color: "green"});
     }).fail(function (data) {
       console.error(data);
       console.error('fail');
