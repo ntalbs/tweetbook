3eaf456d3441e54061393d8d49412b7b98f17e7c
ntalbs <ntalbsen@gmail.com>
2014-07-14 09:44:33 +0900

    tweet.clj: 트윗 기능 추가.

---
+++ src/tweetbook/models/tweet.clj
@@ -0,0 +1,23 @@
+(ns tweetbook.models.tweet
+  (:require [tweetbook.models.config :as config])
+  (:use [twitter.oauth]
+        [twitter.callbacks]
+        [twitter.callbacks.handlers]
+        [twitter.api.restful]
+        [overtone.at-at]
+        [tweetbook.models.db]))
+
+(def thread-pool (mk-pool))
+(def tweet-interval config/tweet-interval)
+(def tweetbot-creds
+  (let [creds config/oauth-settings]
+    (make-oauth-creds (creds :consumer-key)
+                      (creds :consumer-secret)
+                      (creds :access-token)
+                      (creds :access-token-secret))))
+
+(defn tweet [msg]
+  (statuses-update :oauth-creds tweetbot-creds :params {:status msg}))
+
+(defn tweet-random []
+  (tweet (random-msg)))
