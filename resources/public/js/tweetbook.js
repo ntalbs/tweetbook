42a4715da50e0a19625cd04328527cfdf2929754
ntalbs <ntalbsen@gmail.com>
2014-07-14 19:15:33 +0900

    코드 정리.

--- resources/public/js/tweetbook.js
+++ resources/public/js/tweetbook.js
@@ -1,11 +1,15 @@
 $(function () {
-  $('#msg').focus();
-
   var update = function () {
     var msgCnt = $('#msg').val().length,
         srcCnt = $('#src').val().length,
         count = msgCnt + srcCnt + (srcCnt > 0 ? 1 : 0);
     $('#count').text(140 - count);
+  };
+
+  var clear = function () {
+    $('#msg').val('').focus();
+    $('#src').val('');
+    $('#tweet-immediately').val('');
   };
 
   $('#msg, #src').on('keyup', function (e) { update(); });
@@ -16,21 +20,25 @@ $(function () {
       src: $('#src').val(),
       "tweet-immediately":  $('#tweet-immediately').val()
     };
+
     var $listItem = $('<div class="alert alert-dismissible"><button type="button" class="close" data-dismiss="alert"><span aria-hidden="true">&times;</span><span class="sr-only">Close</span></button>'+quote.msg+'<br>'+quote.src +'</div>');
 
     $listItem.appendTo($('#list-container')).css({color: "gray"});
 
     $.post("/addmesg", quote).done(function (data) {
       console.log(data);
-      $('#msg').val('').focus();
-      $('#src').val('');
-      $('#tweet-immediately').val('');
+      clear();
       $listItem.addClass('alert-success');
     }).fail(function (data) {
       $listItem.addClass('alert-danger');
       console.error(data);
       console.error('fail');
+    }).always(function () {
+      update();
     });
     return false;
   });
+
+  clear();
+  $('#msg').focus();
 });
