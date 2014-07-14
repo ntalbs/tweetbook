93c272d77e9d40ceb052e74f35d7ac3e1ccf85c5
ntalbs <ntalbsen@gmail.com>
2014-07-14 09:44:11 +0900

    tweet oauth 설정 추가.

--- src/tweetbook/models/_config.clj
+++ src/tweetbook/models/_config.clj
@@ -1,3 +1,11 @@
 (ns tweetbook.models.config)
 
 (def db-uri "mongodb-uri")
+
+(def oauth-settings
+  {:consumer-key "consumer-key"
+   :consumer-secret "consumer-secret"
+   :access-token "access-token"
+   :access-token-secret "access-token-secret"})
+
+(def tweet-interval (* 1000 60 60 2))
