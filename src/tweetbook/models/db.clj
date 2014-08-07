c1f254e38ffd3bab42f4a542496a50ad8be4f50c
ntalbs <ntalbsen@gmail.com>
2014-08-08 08:00:01 +0900

    DB커넥션 변경

--- src/tweetbook/models/db.clj
+++ src/tweetbook/models/db.clj
@@ -4,19 +4,23 @@
             [monger.query :as mq]
             [tweetbook.models.config :as config]))
 
-(def db (:db (mg/connect-via-uri config/db-uri)))
-
 (defn insert-msg [msg]
-  (mc/insert db "quotes" msg))
+  (let [{:keys [conn db]} (mg/connect-via-uri config/db-uri)]
+    (mc/insert db "quotes" msg)
+    (mg/disconnect conn)))
 
 (defn list-msgs []
-  (println (mc/find-maps db "quotes")))
+  (let [{:keys [conn db]} (mg/connect-via-uri config/db-uri)]
+    (println (mc/find-maps db "quotes"))
+    (mg/disconnect conn)))
 
 (defn random-msg []
-  (let [cnt (mc/count db "quotes")
+  (let [{:keys [conn db]} (mg/connect-via-uri config/db-uri)
+        cnt (mc/count db "quotes")
         rnd (rand-int cnt)
         {:keys [msg src]} (first (mq/with-collection db "quotes"
                                    (mq/skip rnd)
                                    (mq/limit 1)
                                    (mq/snapshot)))]
+    (mg/disconnect conn)
     (str msg "\n" src)))
