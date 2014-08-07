9777501312e969e898a0295f77fb5f2599790ea7
ntalbs <ntalbsen@gmail.com>
2014-08-07 16:22:54 +0900

    설정 파일 변경. 환경 변수에서 설정 정보를 가져오도록 수정. _config.clj 제거.

---
+++ src/tweetbook/models/config.clj
@@ -0,0 +1,15 @@
+(ns tweetbook.models.config)
+
+(def db-uri (System/getenv "TWEETBOOK_DB_URI"))
+
+(def oauth-settings
+  {:consumer-key (System/getenv "TWEETBOOK_TWITTER_CONSUMER_KEY"),
+   :consumer-secret (System/getenv "TWEETBOOK_TWITTER_CONSUMER_SECRET"),
+   :access-token (System/getenv "TWEETBOOK_TWITTER_ACCESS_TOKEN"),
+   :access-token-secret (System/getenv "TWEETBOOK_TWITTER_ACCESS_TOKEN_SECRET")})
+
+(def tweet-interval (* 1000 60 60 2))
+
+(def user
+  {:id (System/getenv "TWEETBOOK_ID")
+   :pw-hash (System/getenv "TWEETBOOK_PW_HASH")})
