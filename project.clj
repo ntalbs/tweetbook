5445bc8032291957a69e7d7a6ce746c9066b54f7
ntalbs <ntalbsen@gmail.com>
2016-04-01 22:30:44 +0100

    Update dependencies.

--- project.clj
+++ project.clj
@@ -1,16 +1,17 @@
 (defproject tweetbook "0.1.0"
   :description "Tweet snippets from books"
   :min-lein-version "2.0.0"
-  :dependencies [[org.clojure/clojure "1.6.0"]
-                 [compojure "1.3.1"]
-                 [liberator "0.12.2"]
-                 [cheshire "5.4.0"]
-                 [ring-server "0.3.1"]
-                 [lib-noir "0.9.5"]
-                 [twitter-api "0.7.7"]
+  :dependencies [[org.clojure/clojure "1.8.0"]
+                 [compojure "1.5.0"]
+                 [liberator "0.14.1"]
+                 [cheshire "5.5.0"]
+                 [ring-server "0.4.0"]
+                 [lib-noir "0.9.9"]
+                 [twitter-api "0.7.8"]
+                 [clj-http "2.1.0"]
                  [overtone/at-at "1.2.0"]
-                 [com.novemberain/monger "2.0.0"]]
-  :plugins [[lein-ring "0.8.10"]]
+                 [com.novemberain/monger "3.0.1"]]
+  :plugins [[lein-ring "0.9.7"]]
   :ring {:handler tweetbook.handler/app
          :init tweetbook.handler/init
          :destroy tweetbook.handler/destroy}
@@ -20,4 +21,4 @@
    {:ring
     {:open-browser? false, :stacktraces? false, :auto-reload? false}}
    :dev
-   {:dependencies [[ring-mock "0.1.5"] [ring/ring-devel "1.2.1"]]}})
+   {:dependencies [[ring/ring-mock "0.3.0"] [ring/ring-devel "1.4.0"]]}})
